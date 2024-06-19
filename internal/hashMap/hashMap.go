78844c07df079b3ecca73278cdc4c647e3c5496d
Delavalom <hi@delavalom.com>
2024-06-18 23:35:34 -0400

    Refactor hashMap package to use HashMap type instead of map[string]*Tree

--- internal/hashMap/hashMap.go
+++ internal/hashMap/hashMap.go
@@ -1,9 +1,5 @@
 package hashMap
 
-import (
-	"crypto/sha1"
-)
-
 type Tree struct {
 	HasSubTree  bool             `json:"hasSubTree"`
 	SubTree     map[string]*Tree `json:"subTree"`     // nil if HasSubTree is false
@@ -12,21 +8,8 @@ type Tree struct {
 	FileContent string           `json:"fileContent"` // empty if HasSubTree is true
 }
 
-type Files map[string]*File
+type HashMap map[string]*Tree
 
-type File struct {
-	IsDir   bool
-	Name    string
-	Content string           // empty if IsDir is true
-	Trees   map[string]*File // empty if IsDir is false
-}
-
-func New() map[string]*Tree {
-	return make(map[string]*Tree)
-}
-
-func Store(content []byte) {
-	hash := sha1.New()
-	hash.Write(content)
-
+func New() HashMap {
+	return make(HashMap)
 }
