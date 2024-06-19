fa7d7d1f96835d3d4d82f9517022e6ad0a737037
Delavalom <hi@delavalom.com>
2024-06-18 23:34:56 -0400

    chore: Store commit snapshot in a file and update index

--- internal/cmd/commit/commit.go
+++ internal/cmd/commit/commit.go
@@ -1,9 +1,12 @@
 package commit
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
+	"github.com/delavalom/dar/internal/hash"
+	"github.com/delavalom/dar/internal/hashMap"
 	"github.com/delavalom/dar/internal/snapshot"
 	"github.com/spf13/cobra"
 )
@@ -21,6 +24,8 @@ func NewCommitCommand() *cobra.Command {
 			}
 
 			fmt.Println("Committing changes to the repository...")
+
+			// Create a file to store the commit message
 			file, err := os.Create(".dar/COMMIT_EDITMSG")
 			if err != nil {
 				panic(err)
@@ -31,15 +36,58 @@ func NewCommitCommand() *cobra.Command {
 				panic(err)
 			}
 
+			// Read the tree snapshot from the tmp file
+			treeBytes, err := os.ReadFile(".dar/tmp")
+			if err != nil {
+				panic(err)
+			}
+
+			tree := hashMap.New()
+
+			if err := json.Unmarshal(treeBytes, &tree); err != nil {
+				panic(err)
+			}
+
+			key := hash.New(treeBytes)
+
+			// Create a snapshot of the repository
+			shot := snapshot.New(string(key), tree, msg)
+
+			// marshal the snapshot to bytes
+			snapshotBytes, err := json.Marshal(shot)
+			if err != nil {
+				panic(err)
+			}
+
+			// Create a file to store the snapshot of the tree
+			snapshotFile, err := os.Create(fmt.Sprintf(".dar/snapshots/%s", key))
+			if err != nil {
+				panic(err)
+			}
+
+			// Write the snapshot to the snapshot file
+			if _, err = snapshotFile.Write(snapshotBytes); err != nil {
+				panic(err)
+			}
+
+			if err = snapshotFile.Close(); err != nil {
+				panic(err)
+			}
+
+			// Create a file to store the commit key
 			indexFile, err := os.Create(".dar/index")
 			if err != nil {
 				panic(err)
 			}
 			defer indexFile.Close()
 
-			shot := snapshot.New("key", nil, "Adding new file")
+			// Write the key of the snapshot to the index file
+			if _, err = indexFile.Write([]byte(key)); err != nil {
+				panic(err)
+			}
 
-			if _, err = indexFile.Write([]byte(shot.Key)); err != nil {
+			// Remove the tmp file
+			if err = os.Remove(".dar/tmp"); err != nil {
 				panic(err)
 			}
 
